35d0bf1c1fb516876585331cd358cee90ab11321
Dan Erwin Magno <danerwin111@gmail.com>
2025-02-20 03:53:05 +0800

    SideBar Component

---
+++ src/component/SideBar.vue
@@ -0,0 +1,21 @@
+<script setup>
+import { watch } from "vue";
+
+const isOpen = defineModel({});
+
+watch(
+  () => isOpen,
+  (newVal) => {
+    console.log(newVal);
+  }
+);
+</script>
+
+<template>
+  <div
+    class="absolute top-0 left-0 translate-x-[-100%] w-[80%] h-full z-10 flex transition-all duration-400 ease-in-out"
+    :class="[isOpen ? 'translate-x-[0%]' : 'translate-x-[-100%]']"
+  >
+    <nav class="w-full max-w-[384px] h-full z-10 bg-red-500"></nav>
+  </div>
+</template>
--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -1,23 +1,40 @@
 <script setup>
-import {
-  IonPage,
-  IonHeader,
-  IonToolbar,
-  IonTitle,
-  IonContent,
-} from "@ionic/vue";
+import HeaderNav from "@/component/HeaderNav.vue";
+import SideBar from "@/component/SideBar.vue";
+import { IonPage, IonContent } from "@ionic/vue";
+import { ref } from "vue";
 import { RouterView } from "vue-router";
+
+const isNavOpen = ref(false);
 </script>
 
 <template>
-  <ion-page>
-    <ion-header>
-      <ion-toolbar>
-        <ion-title>MY App</ion-title>
-      </ion-toolbar>
-    </ion-header>
-    <ion-content>
-      <router-view></router-view>
+  <ion-page
+    class="max-w-[480px] max-h-auto sm:max-h-[850px] mx-auto mt-0 sm:mt-14 relative overflow-hidden"
+  >
+    <div
+      class="bg-black w-full h-full absolute z-10 transition-all duration-400 ease-in-out"
+      :class="[
+        isNavOpen
+          ? 'translate-x-[80%] opacity-40 pointer-events-auto'
+          : 'translate-x-[0%] opacity-0 pointer-events-none',
+      ]"
+      @click="isNavOpen = !isNavOpen"
+    ></div>
+    <SideBar v-model="isNavOpen" />
+    <HeaderNav
+      v-model="isNavOpen"
+      class="transition-transform duration-400 ease-in-out"
+      :class="[isNavOpen ? 'translate-x-[80%]' : 'translate-x-[0%]']"
+    />
+
+    <ion-content
+      class="transition-transform duration-400 ease-in-out"
+      :class="[isNavOpen ? 'translate-x-[80%]' : 'translate-x-[0%]']"
+    >
+      <div class="px-3 pb-5 pt-2">
+        <router-view></router-view>
+      </div>
     </ion-content>
   </ion-page>
 </template>
