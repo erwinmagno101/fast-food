5cbf5f69392c9f35e4b47bee450655daebf56595
Dan Erwin Magno <danerwin111@gmail.com>
2025-02-20 21:59:35 +0800

    Create SectionBlock.vue

---
+++ src/components/SectionBlock.vue
@@ -0,0 +1,25 @@
+<script setup>
+const props = defineProps({
+  heading: String,
+});
+</script>
+
+<template>
+  <section class="space-y-3">
+    <div class="flex flex-row justify-between items-end">
+      <div class="text-2xl font-semibold">{{ heading }}</div>
+      <div>
+        <slot name="options"></slot>
+      </div>
+    </div>
+    <div class="overflow-x-auto remove-scrollbar">
+      <slot />
+    </div>
+  </section>
+</template>
+
+<style scoped>
+.remove-scrollbar::-webkit-scrollbar {
+  display: none;
+}
+</style>
