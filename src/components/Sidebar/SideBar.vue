f60dacded5490adc7eb6a1e207a2888821eb798a
Dan Erwin Magno <danerwin111@gmail.com>
2025-02-21 07:31:41 +0800

    Create SideBar.vue

---
+++ src/components/Sidebar/SideBar.vue
@@ -0,0 +1,15 @@
+<script setup>
+import ProfileSection from "./ProfileSection.vue";
+import NavigationSection from "./NavigationSection.vue";
+import PrimaryBtn from "../PrimaryBtn.vue";
+</script>
+
+<template>
+  <div class="h-full display flex flex-col gap-5">
+    <ProfileSection />
+    <NavigationSection />
+    <div class="mt-auto w-[60%]">
+      <PrimaryBtn> Logout </PrimaryBtn>
+    </div>
+  </div>
+</template>
