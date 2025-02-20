1adc008108ec725dc05d97af78bda318e177fdd4
Dan Erwin Magno <danerwin111@gmail.com>
2025-02-21 07:31:53 +0800

    Create CustomHeader.vue

---
+++ src/components/CustomHeader.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="flex gap-5 mb-5 items-center">
+    <div>
+      <slot name="leading" />
+    </div>
+    <div class="flex-1">
+      <slot />
+    </div>
+    <div>
+      <slot name="trailing" />
+    </div>
+  </div>
+</template>
