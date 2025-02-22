86448606b3408c8c923674d32245dedf34db1d95
Dan Erwin Magno <danerwin111@gmail.com>
2025-02-22 18:18:53 +0800

    Update index.js

--- src/router/index.js
+++ src/router/index.js
@@ -2,11 +2,13 @@ import { createRouter, createWebHistory } from "@ionic/vue-router";
 import HomePage from "../views/HomePage.vue";
 import FoodsPage from "../views/FoodsPage.vue";
 import RoyaltyPage from "../views/RoyaltyPage.vue";
+import OrderPage from "../views/OrderPage.vue";
 
 const routes = [
   { path: "/", name: "home", component: HomePage },
   { path: "/foods", name: "foods", component: FoodsPage },
   { path: "/royalty", name: "royalty", component: RoyaltyPage },
+  { path: "/foods/order", name: "order", component: OrderPage },
 ];
 
 const router = createRouter({
