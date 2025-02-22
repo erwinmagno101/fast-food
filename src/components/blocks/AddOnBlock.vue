02fb5de50cd586e61eba204d54c5f8ddfabab429
Dan Erwin Magno <danerwin111@gmail.com>
2025-02-22 22:17:29 +0800

    Create AddOnBlock.vue

---
+++ src/components/blocks/AddOnBlock.vue
@@ -0,0 +1,67 @@
+<script setup>
+import { PlusIcon, MinusIcon } from "@heroicons/vue/24/solid";
+import { watch, ref } from "vue";
+
+const props = defineProps({
+  data: Object,
+});
+
+const items = defineModel();
+const checked = ref(false);
+const price = ref(0);
+
+price.value = props.data.price;
+const count = ref(1);
+
+watch(
+  () => checked.value,
+  (newVal) => {
+    if (newVal) {
+      items.value.push({ data: props.data, count: count.value });
+    } else {
+      const temp = items.value.filter((item) => item.data === props.data);
+      items.value = temp;
+    }
+  }
+);
+
+watch(
+  () => count.value,
+  (newVal) => {
+    price.value = props.data.price * newVal;
+  }
+);
+</script>
+<template>
+  <div class="flex bg-gray-200 rounded-2xl gap-5 px-5 py-3">
+    <div class="w-24 h-20 bg-red-500"></div>
+    <div class="flex flex-col w-full">
+      <div class="flex justify-bewteen items-start w-full">
+        <div class="flex-1">
+          <div class="font-semibold">{{ data.name }}</div>
+          <div class="text-xs text-gray-500 mb-3">{{ data.category }}</div>
+        </div>
+        <input type="checkbox" v-model="checked" />
+      </div>
+      <div class="flex text-red-600 font-bold gap-3 justify-between mt-auto">
+        <div>P {{ price }}</div>
+
+        <div class="flex gap-3 items-center">
+          <div
+            class="p-2 bg-gray-300 w-fit rounded-xl"
+            @click="() => (count !== 1 ? (count = count - 1) : '')"
+          >
+            <MinusIcon class="w-4 text-red-600" />
+          </div>
+          <div class="text-xs">{{ count }}</div>
+          <div
+            class="p-2 bg-gray-300 w-fit rounded-xl"
+            @click="count = count + 1"
+          >
+            <PlusIcon class="w-4 text-red-600" />
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
