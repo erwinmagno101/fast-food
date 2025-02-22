85a31d25aa3cfb5936526776fefb413e1c7f8f24
Dan Erwin Magno <danerwin111@gmail.com>
2025-02-23 01:22:44 +0800

    Update index.js

--- src/router/index.js
+++ src/router/index.js
@@ -3,12 +3,20 @@ import HomePage from "../views/HomePage.vue";
 import FoodsPage from "../views/FoodsPage.vue";
 import RoyaltyPage from "../views/RoyaltyPage.vue";
 import OrderPage from "../views/OrderPage.vue";
+import PaymentPage from "../views/PaymentPage.vue";
+import SuccessPage from "../views/SuccessPage.vue";
 
 const routes = [
   { path: "/", name: "home", component: HomePage },
   { path: "/foods", name: "foods", component: FoodsPage },
   { path: "/royalty", name: "royalty", component: RoyaltyPage },
   { path: "/foods/order", name: "order", component: OrderPage },
+  { path: "/foods/order/payment", name: "payment", component: PaymentPage },
+  {
+    path: "/foods/order/payment/success",
+    name: "success",
+    component: SuccessPage,
+  },
 ];
 
 const router = createRouter({
