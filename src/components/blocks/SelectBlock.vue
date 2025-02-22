3dc7be7edb7e9b295e55193fb10b0d14372fba2b
Dan Erwin Magno <danerwin111@gmail.com>
2025-02-22 18:18:18 +0800

    Create SelectBlock.vue

---
+++ src/components/blocks/SelectBlock.vue
@@ -0,0 +1,77 @@
+<script setup>
+import { ChevronDownIcon } from "@heroicons/vue/24/solid";
+import { onMounted, onUnmounted, ref, watch } from "vue";
+const componentRef = ref(null);
+import { defineModel } from "vue";
+
+const price = defineModel();
+
+const items = [
+  { name: "Coke", price: 25 },
+  { name: "Iced Tea", price: 30 },
+];
+
+const selected = ref(0);
+
+const isOpen = ref(false);
+
+watch(
+  () => selected.value,
+  (newVal) => {
+    price.value = items[selected.value].price;
+  }
+);
+
+const handleClickOutside = (e) => {
+  if (!componentRef.value.contains(e.target) && isOpen.value) {
+    isOpen.value = false;
+  }
+};
+
+watch(
+  () => isOpen.value,
+  (newVal) => {
+    if (newVal) {
+      document.addEventListener("click", handleClickOutside);
+    } else {
+      document.removeEventListener("click", handleClickOutside);
+    }
+  }
+);
+
+onMounted(() => {
+  price.value = items[selected.value].price;
+});
+
+onUnmounted(() => {
+  document.removeEventListener("click", handleClickOutside);
+});
+</script>
+
+<template>
+  <div
+    ref="componentRef"
+    class="px-4 py-3 rounded-2xl bg-gray-100 border border-gray-300"
+    @click="isOpen = !isOpen"
+  >
+    <div class="flex justify-between items-center">
+      <div>
+        <div class="text-xs text-gray-500">Choose Beverages</div>
+        <div class="texat-sm">{{ items[selected].name }}</div>
+      </div>
+      <ChevronDownIcon class="w-5 text-red-600" />
+    </div>
+    <div
+      class="absolute bottom-0 left-0 w-full translate-y-[100%] bg-gray-100 p-2 px-3 space-y-2 border border-gray-300 rounded-2xl shadow-xl"
+      v-if="isOpen"
+    >
+      <div
+        v-for="(item, index) in items"
+        :key="index"
+        @click="selected = index"
+      >
+        {{ item.name }}
+      </div>
+    </div>
+  </div>
+</template>
