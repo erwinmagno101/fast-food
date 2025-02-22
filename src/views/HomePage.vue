33e017527efe57c590b4040a9ee75847c358fdd2
Dan Erwin Magno <danerwin111@gmail.com>
2025-02-22 15:28:25 +0800

    Update HomePage.vue

--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -1,18 +1,25 @@
 <script setup>
-import CategoryBlock from "@/components/CategoryBlock.vue";
-import PrimaryBtn from "@/components/PrimaryBtn.vue";
-import SearchBar from "@/components/SearchBar.vue";
-import SectionBlock from "@/components/SectionBlock.vue";
-import VoucherBlock from "@/components/VoucherBlock.vue";
+import CategoryBlock from "@/components/blocks/CategoryBlock.vue";
+import PrimaryBtn from "@/components/ui/PrimaryBtn.vue";
+import SearchBar from "@/components/ui/SearchBar.vue";
+import SectionBlock from "@/components/ui/SectionBlock.vue";
+import VoucherBlock from "@/components/blocks/VoucherBlock.vue";
 import { ChevronRightIcon } from "@heroicons/vue/24/solid";
-import { IonGrid, IonCol, IonMenuButton, IonRow } from "@ionic/vue";
-import CustomHeader from "@/components/CustomHeader.vue";
-import ProfileAvatar from "@/components/ProfileAvatar.vue";
-import RoyaltyPoint from "@/components/Sidebar/RoyaltyPoint.vue";
+import {
+  IonGrid,
+  IonCol,
+  IonMenuButton,
+  IonRow,
+  IonPage,
+  IonContent,
+} from "@ionic/vue";
+import CustomHeader from "@/components/ui/CustomHeader.vue";
+import ProfileAvatar from "@/components/ui/ProfileAvatar.vue";
+import RoyaltyPoint from "@/components/sidebar/RoyaltyPoint.vue";
 import { useFoodStore } from "@/composables/foodstore";
-import FoodItemBlock from "@/components/FoodItemBlock.vue";
+import FoodItemBlock from "@/components/blocks/FoodItemBlock.vue";
 import { onMounted, ref } from "vue";
-import { useRoute, useRouter } from "vue-router";
+import { useRouter } from "vue-router";
 
 const foodstore = useFoodStore();
 const router = useRouter();
@@ -35,82 +42,92 @@ onMounted(() => {
 </script>
 
 <template>
-  <div class="mb-24">
-    <CustomHeader>
-      <template #leading>
-        <ion-menu-button class="text-red-600"></ion-menu-button>
-      </template>
+  <ion-page>
+    <ion-content class="ion-padding">
+      <div class="mb-24">
+        <CustomHeader>
+          <template #leading>
+            <ion-menu-button class="text-red-600"></ion-menu-button>
+          </template>
 
-      <RoyaltyPoint />
+          <RoyaltyPoint />
 
-      <template #trailing>
-        <ProfileAvatar class="w-10 h-10" />
-      </template>
-    </CustomHeader>
-    <div class="space-y-10">
-      <div class="flex gap-3 justify-center items-center">
-        <div class="w-28">
-          <img src="../assets/images/logo.webp" />
-        </div>
-        <div>
-          <div>Anneoyong, Chou!</div>
-          <div class="text-3xl font-bold">What do you want to eat?</div>
-        </div>
-      </div>
-
-      <SearchBar placeholder="try our new Steak Fries Veggies" />
-
-      <SectionBlock heading="Special Offers!">
-        <template #options>
-          <div class="flex gap-2 text-red-600">
-            <div class="font-semibold">View All</div>
-            <ChevronRightIcon class="w-4" />
+          <template #trailing>
+            <ProfileAvatar class="w-10 h-10" />
+          </template>
+        </CustomHeader>
+        <div class="space-y-10">
+          <div class="flex gap-3 justify-center items-center">
+            <div class="w-28">
+              <img src="../assets/images/logo.webp" />
+            </div>
+            <div>
+              <div>Anneoyong, Chou!</div>
+              <div class="text-3xl font-bold">What do you want to eat?</div>
+            </div>
           </div>
-        </template>
-        <div class="flex gap-3">
-          <VoucherBlock v-for="i in 3" :key="i" />
-        </div>
-      </SectionBlock>
 
-      <SectionBlock heading="Category">
-        <div class="flex gap-3">
-          <CategoryBlock
-            v-for="i in 5"
-            :key="i"
-            :title="i + ' Title Here'"
-            :subtitle="i + 5 + ' Recipies'"
-          />
-        </div>
-      </SectionBlock>
+          <SearchBar placeholder="try our new Steak Fries Veggies" />
 
-      <SectionBlock heading="What's New?">
-        <div class="max-w-[480px] h-56 bg-red-600 rounded-2xl"></div>
-      </SectionBlock>
-
-      <SectionBlock heading="Most Popular">
-        <ion-grid>
-          <ion-row>
-            <ion-col
-              size="6"
-              size-sm="auto"
-              v-for="food in displayedFoods"
-              :key="food.id"
-            >
-              <div class="p-1">
-                <FoodItemBlock :key="food.id" :data="food" />
+          <SectionBlock heading="Special Offers!">
+            <template #options>
+              <div class="flex gap-2 text-red-600">
+                <div class="font-semibold">View All</div>
+                <ChevronRightIcon class="w-4" />
               </div>
-            </ion-col>
-          </ion-row>
-        </ion-grid>
-      </SectionBlock>
-      <div class="fixed bottom-10 right-5">
-        <PrimaryBtn
-          class="py-5 px-12 w-fit shadow-lg"
-          @click="navigate('foods')"
-        >
-          Order Now!
-        </PrimaryBtn>
+            </template>
+            <div class="flex gap-3">
+              <VoucherBlock v-for="i in 3" :key="i" />
+            </div>
+          </SectionBlock>
+
+          <SectionBlock heading="Category">
+            <div class="flex gap-3">
+              <CategoryBlock
+                v-for="i in 5"
+                :key="i"
+                :title="i + ' Title Here'"
+                :subtitle="i + 5 + ' Recipies'"
+              />
+            </div>
+          </SectionBlock>
+
+          <SectionBlock heading="What's New?">
+            <div class="max-w-[480px] h-56 bg-red-600 rounded-2xl"></div>
+          </SectionBlock>
+
+          <SectionBlock heading="Most Popular">
+            <ion-grid>
+              <ion-row>
+                <ion-col
+                  size="6"
+                  size-sm="auto"
+                  v-for="food in displayedFoods"
+                  :key="food.id"
+                >
+                  <div class="p-1">
+                    <FoodItemBlock :key="food.id" :data="food" />
+                  </div>
+                </ion-col>
+              </ion-row>
+            </ion-grid>
+          </SectionBlock>
+          <div class="fixed bottom-10 right-5">
+            <PrimaryBtn
+              class="py-5 px-12 w-fit shadow-lg"
+              @click="navigate('foods')"
+            >
+              Order Now!
+            </PrimaryBtn>
+          </div>
+        </div>
       </div>
-    </div>
-  </div>
+    </ion-content>
+  </ion-page>
 </template>
+
+<style scoped>
+ion-content::part(scroll) {
+  padding-top: var(--ion-safe-area-top, 0);
+}
+</style>
