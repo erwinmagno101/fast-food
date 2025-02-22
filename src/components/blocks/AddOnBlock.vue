b6d718a2bcce72794f619fde508b2de1ec22305a
Dan Erwin Magno <danerwin111@gmail.com>
2025-02-23 01:22:37 +0800

    Update AddOnBlock.vue

--- src/components/blocks/AddOnBlock.vue
+++ src/components/blocks/AddOnBlock.vue
@@ -41,7 +41,7 @@ watch(
           <div class="font-semibold">{{ data.name }}</div>
           <div class="text-xs text-gray-500 mb-3">{{ data.category }}</div>
         </div>
-        <input type="checkbox" v-model="checked" />
+        <input type="checkbox" v-model="checked" class="custom-checkbox" />
       </div>
       <div class="flex text-red-600 font-bold gap-3 justify-between mt-auto">
         <div>P {{ price }}</div>
@@ -53,7 +53,7 @@ watch(
           >
             <MinusIcon class="w-4 text-red-600" />
           </div>
-          <div class="text-xs">{{ count }}</div>
+          <div class="text-xs text-black">{{ count }}</div>
           <div
             class="p-2 bg-gray-300 w-fit rounded-xl"
             @click="count = count + 1"
@@ -65,3 +65,32 @@ watch(
     </div>
   </div>
 </template>
+
+<style scoped>
+.custom-checkbox {
+  appearance: none;
+  width: 20px;
+  height: 20px;
+  border: 1px solid rgba(128, 128, 128, 0.226);
+  border-radius: 4px;
+  background-color: white;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  transition: all 0.2s ease-in-out;
+}
+
+.custom-checkbox:checked {
+  background-color: white;
+  border-color: white;
+}
+
+.custom-checkbox:checked::after {
+  content: "✓";
+  color: yellow;
+  font-size: 14px;
+  font-weight: bold;
+  display: block;
+  text-align: center;
+}
+</style>
